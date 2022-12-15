bee05c18f0fdf6285ba2a7f6d9859d92e1beb529
Dyeuse <dyeuse@gmail.com>
2022-12-15 16:30:02 -0500

    Add test env.

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import autoprefixer from "autoprefixer";
@@ -5,6 +7,11 @@ import autoprefixer from "autoprefixer";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./testsSetup.js",
+  },
   css: {
     postcss: {
       plugins: [
