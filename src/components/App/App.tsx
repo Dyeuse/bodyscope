a1fbff20cf0e17cfddb7683890503fd7300e07d3
Dyeuse <dyeuse@gmail.com>
2023-03-20 10:22:14 -0400

    Add LanguageContext

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,6 +2,7 @@ import styles from "./App.module.scss";
 import Form from "../form/Form";
 import CardsDisplayer from "../cardsDisplayer/CardsDisplayer";
 import { get5FakeCards } from "../../utils/fakeCards";
+import LanguageContext from "../../contexts/LanguageContext";
 
 const { mainContainer, appWrapper } = styles;
 function App() {
@@ -9,8 +10,10 @@ function App() {
     <div className={mainContainer}>
       <div className={appWrapper}>
         <h1>BodyScope</h1>
-        <Form />
-        <CardsDisplayer>{get5FakeCards()}</CardsDisplayer>
+        <LanguageContext.Provider value="french">
+          <Form />
+          <CardsDisplayer>{get5FakeCards()}</CardsDisplayer>
+        </LanguageContext.Provider>
       </div>
     </div>
   );
