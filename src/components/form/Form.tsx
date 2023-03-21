fffc54bd05ea06e45ad7f6967e1c0a0f36165653
Dyeuse <dyeuse@gmail.com>
2023-03-21 09:58:53 -0400

    Update button's Form with LanguageContext

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,17 +1,26 @@
 import Options from "../options/Options";
 import Measurements from "../measurements/Measurements";
+import { useContext } from "react";
+import LanguageContext from "../../contexts/LanguageContext";
 import styles from "./Form.module.scss";
 
 const { formContainer, dataWrapper } = styles;
 
+const buttonTexts: { [index: string]: string } = {
+  english: "calculate",
+  french: "calculer",
+};
+
 function Form() {
+  const language = useContext(LanguageContext);
+
   return (
     <form className={formContainer}>
       <div className={dataWrapper}>
         <Options />
         <Measurements />
       </div>
-      <button type="submit">calculate</button>
+      <button type="submit">{buttonTexts[language]}</button>
     </form>
   );
 }
