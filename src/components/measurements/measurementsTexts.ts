e24c053a48d0934338a09dd730f1694ba5fb5500
Dyeuse <dyeuse@gmail.com>
2023-03-21 09:48:53 -0400

    Add measurementsTexts obj

---
+++ src/components/measurements/measurementsTexts.ts
@@ -0,0 +1,20 @@
+type MeasurementsTextType = {
+  [index: string]: { name: string; title: string; min: number; max: number }[];
+};
+
+const measurementsTexts: MeasurementsTextType = {
+  english: [
+    { name: "height", title: "height (cm)", min: 100, max: 220 },
+    { name: "weight", title: "weight (kg)", min: 30, max: 220 },
+    { name: "waist", title: "waist (cm)", min: 30, max: 220 },
+    { name: "neck", title: "neck (cm)", min: 20, max: 80 },
+  ],
+  french: [
+    { name: "height", title: "taille (cm)", min: 100, max: 220 },
+    { name: "weight", title: "poids (kg)", min: 30, max: 220 },
+    { name: "waist", title: "tour de taille (cm)", min: 30, max: 220 },
+    { name: "neck", title: "cou (cm)", min: 20, max: 80 },
+  ],
+};
+
+export default measurementsTexts;
